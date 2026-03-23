aafb443af9da513173002118ea3df641c2042e08
anish <anishkr0098@gmail.com>
2026-03-23 11:53:33 +0530

    folder structure

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,5 +1,54 @@
 import express from "express"
+import { JWT_SECRET } from "@repo/backend-common/config"
+import jwt from "jsonwebtoken";
+import { middleware } from "./middleware";
+import { CreateUserSchema, SigninSchema, CreateRoomSchema } from "@repo/common/types"
 
 const app = express();
+
+app.post('/signup', (req,res) =>{
+    const data = CreateUserSchema.safeParse(req.body);
+    if(!data.success){
+         res.json({
+            msg: "Incorrect credentials"
+        })
+        return;
+    }
+    //db call
+    
+})
+app.post('/signin', (req,res) =>{
+    const data = SigninSchema.safeParse(req.body);
+    if(!data.success){
+         res.json({
+            msg: "Incorrect credentials"
+        })
+        return;
+    }
+
+    const userId = 1;
+    const token = jwt.sign({
+        userId
+    }, JWT_SECRET);
+    res.json({
+        token
+    })
+
+})
+app.post('/room',middleware, (req,res) =>{
+    const data = CreateRoomSchema.safeParse(req.body);
+    if(!data.success){
+         res.json({
+            msg: "Incorrect credentials"
+        })
+        return;
+    }
+
+    // db call
+    res.json({
+        room:123
+    })
+
+})
 
 app.listen(3001);
--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,8 +1,30 @@
 import { WebSocketServer } from "ws";
+import jwt from "jsonwebtoken";
+import { JWT_SECRET } from "@repo/backend-common/config"
+
+
+interface JwtPayLoad {
+    userId: string;
+}
 
 const wss = new WebSocketServer({ port: 8080});
 
-wss.on('connection', function(ws){
+wss.on('connection', function(ws, request){
+    // extraction of token
+    const url = request.url;
+    if(!url){
+        return;
+    }
+    const queryParams = new URLSearchParams(url.split('?')[1]);
+    const token = queryParams.get('token') || "";
+    const decoded = jwt.verify(token, JWT_SECRET);
+
+
+    if(!decoded || !(decoded as JwtPayLoad).userId){
+        ws.close();
+        return;
+    }
+
     ws.on('message', function message(data){
         ws.send('pong')
     });
