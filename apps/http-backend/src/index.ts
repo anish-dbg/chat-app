50327f33dd28bb6fa823d9e46cc20bcbbb395ce9
anish <anishkr0098@gmail.com>
2026-03-24 11:03:57 +0530

    signup

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,20 +1,44 @@
 import express from "express"
 import { JWT_SECRET } from "@repo/backend-common/config"
+import { prismaClient } from "@repo/db/client"
 import jwt from "jsonwebtoken";
 import { middleware } from "./middleware";
 import { CreateUserSchema, SigninSchema, CreateRoomSchema } from "@repo/common/types"
 
 const app = express();
 
-app.post('/signup', (req,res) =>{
-    const data = CreateUserSchema.safeParse(req.body);
-    if(!data.success){
+app.post('/signup', async (req,res) =>{
+    const parseData = CreateUserSchema.safeParse(req.body);
+    if(!parseData.success){
          res.json({
             msg: "Incorrect credentials"
         })
         return;
     }
+
     //db call
+    try{
+        const user = await prismaClient.user.create({
+        data: {
+            email: parseData.data?.username,
+            password: parseData.data.password,
+            name: parseData.data.name
+        }
+    });
+
+    const token = jwt.sign({
+        userId: user.id
+    }, JWT_SECRET);
+
+    res.json({
+        token
+    })
+    } catch(e) {
+        res.status(411).json({
+            msg: "User already exists with this username"
+        })
+    }
+    
     
 })
 app.post('/signin', (req,res) =>{
