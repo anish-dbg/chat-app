3520d1aa28af8542ca28859ccd91f76081cefa40
anish <anishkr0098@gmail.com>
2026-03-25 20:39:29 +0530

    frontend

---
+++ apps/web/app/room/[roomId]/page.tsx
@@ -0,0 +1,26 @@
+import axios from "axios";
+import { BACKEND_URL } from "../../config";
+import { ChatRoom } from "../../../components/ChatRoom";
+
+
+async function getRoomId(slug: string){
+   const response = await axios.get(`${BACKEND_URL}/room/${slug}`);
+
+   if (!response.data.room) {
+      throw new Error("Room not found");
+   }
+
+   return response.data.room.id;
+}
+
+
+export default async function ChatRoom1({ params }: { params: { slug: string } }) {
+    try {
+        const slug = params.slug;
+        const roomId = await getRoomId(slug);
+
+        return <ChatRoom id={roomId} />;
+    } catch (err) {
+        return <div>Room not found</div>;
+    }
+}
